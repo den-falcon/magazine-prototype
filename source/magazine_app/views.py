43e0f66321158e2132f869f4e3d746491ee79443
denis-sokolov <den.s.glad@gmail.com>
2022-01-13 00:21:30 +0300

    Созданна стартовая страница index.html создан шаблон формы, создан базовый шаблон

--- source/magazine_app/views.py
+++ source/magazine_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.filter(remainder__gt=0).order_by('category', 'name')
+    return render(request, 'index.html', context={'products': products})
