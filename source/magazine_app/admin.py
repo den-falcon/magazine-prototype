043bb7da7de5d4e62b7a544faef209557eea53b1
denis-sokolov <den.s.glad@gmail.com>
2022-01-12 22:57:52 +0300

    Созданна модель БД, выполненна миграция, БД зарегистрированна в админ панели

--- source/magazine_app/admin.py
+++ source/magazine_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+    list_filter = ['name']
+    search_fields = ['name', 'category']
+    fields = ['name', 'description', 'category', 'remainder', 'price']
+    # readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/magazine_app/models.py
+++ source/magazine_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [('other', 'Прочее'), ('clothes', 'Одежда'), ('electronics', 'Электронника'),
+                    ('for_home', 'Для дома'), ('accessories', 'Аксессуары')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='other', verbose_name='Категория')
+    remainder = models.IntegerField(null=True, blank=True, verbose_name='Остаток')
+    price = models.DecimalField(max_length=10, max_digits=9, decimal_places=2, null=False, blank=False,
+                                verbose_name='Стоимость')
